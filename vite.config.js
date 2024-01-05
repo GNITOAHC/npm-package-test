8688c5c157eb114618712d3b355674696b23bda0
GINTOAHC <chaotingchen10@gmail.com>
2024-01-06 00:39:44 +0800

    dev: Add ts support and other configs

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
-export default defineConfig ({
+export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "package-test",
-   		 
-      fileName: "index",
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'package-test',
+
+      fileName: 'index',
     },
     rollupOptions: {
-      external: ["react"],
+      external: ['react'],
     },
   },
-});
+  plugins: [dts({ rollupTypes: true })],
+})
